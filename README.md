91534fbe47993a6ed5d87acb4f37f9bb07f2b2c6
shareed <shanreed6@gmail.com>
2020-07-08 17:06:34 -0700

    added function to get random colors each time the page reloads

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Guessing
+
+#####JavaScript and random numbers
+JavaScript has a built-in function called Math.random that produces a random decimal number between 0 and 1. By multiplying this number, you can get a random number between 0 and any other number. For example, to get a number between 0 and 100, you multiply the result of Math.random by 100, like this:
+
+myRandomNumber = Math.random()*100;
+This works because the smallest random number Math.random can create is 0, and 0 * 100=0. The largest random number is 1, and 1 * 100 = 100. And with any random number in between, you get a number in between 0 and 100. For example, 0.25 * 100 = 25.
+
+Math.random produces a decimal number, for example 0.245834. For your game, you only want whole numbers (integers), because only those work as colour values. So you need to round the result of Math.random. Luckily, JavaScript has a built-in function called Math.round to help you round decimal number. So to get a whole number between 0 and 100, you would write:
+
+myRandomNumber = Math.round(Math.random()*100);
--- scripts.js
+++ scripts.js
@@ -18,9 +18,29 @@ function setButtonColor(button, red, green, blue){
 
 
 //takes the first button in the array and changes the button colour to blue using your
-setButtonColor(buttons[0], 0, 0, 255);
-setButtonColor(buttons[1], 55,255, 25);
-setButtonColor(buttons[2], 134, 89, 255);
-setButtonColor(buttons[3], 255, 255, 0);
-setButtonColor(buttons[4], 0, 255, 255);
-setButtonColor(buttons[5], 255, 0, 255);
+// setButtonColor(buttons[0], 0, 0, 255);
+// setButtonColor(buttons[1], 55,255, 25);
+// setButtonColor(buttons[2], 134, 89, 255);
+// setButtonColor(buttons[3], 255, 255, 0);
+// setButtonColor(buttons[4], 0, 255, 255);
+// setButtonColor(buttons[5], 255, 0, 255);
+
+function makeColourValue(){
+    return Math.round(Math.random()*255);
+}
+
+// var red = makeColourValue();
+// var green = makeColourValue();
+// var blue = makeColourValue();
+
+// setButtonColor(buttons[0], red, green, blue);
+
+for (var i = 0; i < buttons.length; i++) {
+
+    var red = makeColourValue();
+    var green = makeColourValue();
+    var blue = makeColourValue();
+  
+    setButtonColor(buttons[i], red, green, blue);
+  
+  }
